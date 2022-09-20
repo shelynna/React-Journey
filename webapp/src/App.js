fcf589e1b2ef60d21b884aee8fbe41417e6d720b
shelynna mckynhills <51500785+shelynna@users.noreply.github.com>
2022-09-20 21:40:13 +0000

    setups

---
+++ webapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <form>
+        <input placeholder="your Nmae"/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
